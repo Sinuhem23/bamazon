d8df1b7d2ef97240185ba91b04d30cb948f6fe79
sinuhe Montero <sinuhem23@gmail.com>
2019-10-14 01:09:43 -0400

    updated done

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -5,7 +5,7 @@ USE bamazon_DB;
 CREATE TABLE products (
 	item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
-	department_name VARCHAR(30) NOT NULL,
+	department VARCHAR(30) NOT NULL,
 	price INTEGER(10) NOT NULL,
 	stock_quantity INTEGER(10) NOT NULL,
     PRIMARY KEY (item_id)
@@ -13,17 +13,17 @@ CREATE TABLE products (
 );
 
 INSERT INTO products 
-(product_name, department_name, price, stock_quantity)
+(product_name, department, price, stock_quantity)
 VALUES 
-('MacBook-Pro', 'Electronics-Department', 2000, 60),
-('iPad-Pro', 'Electronics-Department', 1000, 20),
-('Apple-TV', 'Electronics-Department', 200, 100),
-('Nike-Socks', 'Apparel-Department', 10, 300),
-('Adidas-Socks', 'Apparel-Department', 10, 200),
-('Puma-Socks', 'Apparel-Department', 8, 100),
-('Abstract-Painting', 'Art-Department', 5000, 2),
-('Painting-Kit', 'Art-Department', 60, 200),
-('Canvas', 'Art-Department', 10, 300),
-('Easels', 'Art', 200, 30);
+('MacBook-Pro', 'Electronics', 60),
+('iPad-Pro', 'Electronics', 20),
+('Apple-TV', 'Electronics', 100),
+('Nike-Socks', 'Apparel', 300),
+('Adidas-Socks', 'Apparel', 200),
+('Puma-Socks', 'Apparel', 100),
+('Abstract-Painting', 'Art', 2),
+('Painting-Kit', 'Art', 200),
+('Canvas', 'Art', 300),
+('Easels', 'Art', 30);
 
 SELECT * FROM products
