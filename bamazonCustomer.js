618fd0d1d8eb64364722ad1c35bd901912dae82b
sinuhe Montero <sinuhem23@gmail.com>
2019-10-13 22:56:18 -0400

    updated

---
+++ bamazonCustomer.js
@@ -0,0 +1,84 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// create connection
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Winter18!',
+    database : 'bamazon_DB'
+   
+});
+// validataInput makes sure that the user is supplying positive INT
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+
+    }else{
+        return 'Whole number is needed, try again.(Can not be zero.)';
+    }
+}
+
+// Item/quantity
+function purchasePrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: "Please enter the ID of the item you desire.",
+            validate: validateInput,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many would you like?',
+            validate: validateInput,
+            filter: Number
+        }
+    ]).then(function(input){
+        var item = input.item_id;
+        var quantity = input.quantity;
+
+        var queryString = 'SELECR * FROM products WHERE ?';
+
+        db.query(queryString, {item_id: item}, function(err, data) {
+            if (err) throw err;
+
+            if (data.length === 0){
+                console.log("ERROR: The Item ID entered is invalid. Please try again.");
+                displayInventory();
+            }else {
+                var productData = data[0];
+                // If quantity requested is in stock
+              if (quantity <= productData.stock_quantity){
+                  console.log ('Your in LUCK! Your requested product is in stock! Order is now in progress.');
+                  
+                //   Updating query string
+
+                var updatingQueryString ='UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+                db.query (updatingQueryString, function(err, data) {
+                    if (err) throw err;
+
+                    console.log('Its READY! your total is $' + productData.price * quantity);
+                    console.log('Thank You! Please come again!');
+                    console.log("\n-----------------------------------------------------------\n");
+
+                    // Ending Database
+                    db.end();
+                })
+                }else {
+                    console.log('OH NO! There is not enough in stock. Your order was not placed.');
+                    console.log('Please retry');
+                    console.log ('\n-------------------------------------------------------\n');
+                    displayInventory();
+                }  
+            }
+        })
+    })
+}
+
---
+++ bamazon_DB.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+USE bamazon_DB;
+
+CREATE TABLE products (
+	id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(30) NOT NULL,
+	price INTEGER(10) NOT NULL,
+	stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+INSERT INTO products 
+(product_name, department_name, price, stock_quantity)
+VALUES 
+('MacBook-Pro', 'Electronics-Department', 2000, 60),
+('iPad-Pro', 'Electronics-Department', 1000, 20),
+('Apple-TV', 'Electronics-Department', 200, 100),
+('Nike-Socks', 'Apparel-Department', 10, 300),
+('Adidas-Socks', 'Apparel-Department', 10, 200),
+('Puma-Socks', 'Apparel-Department', 8, 100),
+('Abstract-Painting', 'Art-Department', 5000, 2),
+('Painting-Kit', 'Art-Department', 60, 200),
+('Canvas', 'Art-Department', 10, 300),
+('Easels', 'Art', 200, 30);
+
+-- SELECT * FROM products
